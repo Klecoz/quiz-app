a680afac4eff333f87a48cf6ca044ecaed29d187
Klecoz <arseniocolon@gmail.com>
2015-07-13 15:47:59 -0400

    Added data structure to javascript file with questions, answers, and the correct answer.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,79 @@
 $( document ).ready(function() {
-  // Handler for .ready() called.
+
+  //Data Structure for quiz questions, answers, and correct answers.
+  var questions = [
+  //Question 1
+  {
+    question: 'What is the science of fermenting beer called?',
+
+    answers: [
+    'Zygotica',
+    'Zymurgy',
+    'Saccormycecea',
+    'Sacromyetica'
+    ],
+
+    correct: 1,
+
+  },
+  //Question 2
+  {
+    question: 'What process determines the composition of Wort?',
+
+    answers: [
+    'Brocking',
+    'Germinating',
+    'Heating',
+    'Mashing'
+    ],
+
+    correct: 3,
+
+  },
+  //Question 3
+  {
+    question: 'What is the oldest active brewery in the world?',
+
+    answers: [
+    'Heineken',
+    'Hefeweizen',
+    'Amstel',
+    'Weihenstephan'
+    ],
+
+    correct: 3,
+
+  },
+  //Question 4
+  {
+    question: 'What is the sweet, amber extract that is filtered from malt called?',
+
+    answers: [
+    'Wort',
+    'Drought',
+    'Germ',
+    'Hops'
+    ],
+
+    correct: 0,
+
+  },
+  //Question 5
+  {
+    question: 'What is the unfermentable sugars left that act as the body of the beer called?',
+
+    answers: [
+    'Hops',
+    'Pilsner',
+    'Dextrin',
+    'Bock'
+    ],
+
+    correct: 2,
+  },
+  ];
+
+
+
+
 });
